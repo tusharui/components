cfb4353a2cdfa6793ceeaba7b7be13dc09f8be77
Tushar <tusharanshu18@gmail.com>
2025-11-06 09:04:15 +0530

    faq

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Counter from "@/components/ui/Counter";
 import FAQ from "@/components/ui/Faq";
+import Searchable from "@/components/ui/Searchable";
 export default function Home() {
   return (
   <div>
-  <Counter/>
+    <Counter/>
   <FAQ/>
+  <Searchable/>
     
   </div>
   );
